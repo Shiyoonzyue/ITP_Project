9c3a39d307c65abf13cea3762acd5862222286c1
HARRY JANE ANAK ANGKU <harry.xinzyue@outlook.com>
2025-12-20 20:44:03 +0800

    Fixed error
    function declaration error fixed

--- main.c
+++ main.c
@@ -1,44 +1,47 @@
 #include <stdio.h>
 
+void mainMenu(void);
+void login(void);
+void registerPage(void);
 
-int main(){	
-	mainMenu();
-	return 0;
+void mainMenu(void){
+    int choice;
+    printf("\n---------------------------------\n");
+    printf("Welcome to Room Allocation System");
+    printf("\n---------------------------------\n\n\n");
+
+    printf("Please Login Or Register\n");
+    printf("1. Login\n");
+    printf("2. Register\n");
+    printf("3. Exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch(choice){
+        case 1:
+            login();
+            break;
+        case 2:
+            registerPage();
+            break;
+        case 3:
+            printf("\nExit");
+            break;
+        default:
+            printf("\nInvalid choice");
+            break;
+    }
 }
 
-void mainMenu(){
-	int choice;
-	printf("\n---------------------------------\n");
-	printf("Welcome to Room Allocation System");
-	printf("\n---------------------------------\n\n\n");
-	
-	printf("Please Login Or Register\n");
-	printf("1.Login\n");
-	printf("2.Register\n");
-	printf("3.Exit\n");
-	printf("Enter your choice: ");
-	scanf("%d", &choice);
-	
-	switch(choice){
-		case 1:
-			login();
-			break;
-		case 2:
-			registerPage();
-			break;
-		case 3:
-			printf("\nExit");
-			break;
-		default:
-			printf("Loop");
-			break;
-	}
+void login(void){
+    printf("\nLogin Page");
 }
 
-void login(){
-	printf("\nLogin Page");
+void registerPage(void){
+    printf("\nRegister Page");
 }
 
-void registerPage(){
-	printf("\nRegister Page");
+int main(void){	
+    mainMenu();
+    return 0;
 }
